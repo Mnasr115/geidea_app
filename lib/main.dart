2acf0ed8bbea329d8715cedb9bdd766ec7b371ef
Mnasr115 <mahmoudinreallife@gmail.com>
2024-12-09 18:27:57 +0200

    add all in only one screen main and modify the checkout data to be EGP and Egypt addresses & make Initialize in same file , before pay

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:geidea_app/home_screen.dart';
-import 'package:geideapay/geideapay.dart';
+import 'package:geideapay/api/response/order_api_response.dart';
+import 'package:geideapay/common/geidea.dart';
+import 'package:geideapay/common/server_environments.dart';
+import 'package:geideapay/models/address.dart';
+import 'package:geideapay/widgets/checkout/checkout_options.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Geidea Demo App',
+      home: PaymentScreen(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  final _plugin = GeideapayPlugin();
+class PaymentScreen extends StatefulWidget {
+  const PaymentScreen({super.key});
+
+  @override
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  final GeideapayPlugin _plugin = GeideapayPlugin();
 
   @override
   void initState() {
     super.initState();
+    _initializeGeideaPlugin();
+  }
+
+  void _initializeGeideaPlugin() {
     _plugin.initialize(
-      publicKey: "<YOUR MERCHANT KEY>",
-      apiPassword: "<YOUR MERCHANT PASSWORD>",
+      publicKey: "<YOUR MERCHANT KEY>", 
+      apiPassword: "<YOUR MERCHANT PASSWORD>", 
       serverEnvironment: ServerEnvironmentModel.EGY_PROD(),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Geidea Demo App',
-      home: HomeScreen(),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Geidea Payment Demo')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await _startPayment(context);
+          },
+          child: const Text('Start Payment'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _startPayment(BuildContext context) async {
+    Address billingAddress = Address(
+      city: "Cairo", 
+      countryCode: "EGY", 
+      street: "Tahrir Square",
+      postCode: "11511", 
+    );
+
+    Address shippingAddress = Address(
+      city: "Alexandria",
+      countryCode: "EGY",
+      street: "Corniche Road",
+      postCode: "21519",
+    );
+
+    CheckoutOptions checkoutOptions = CheckoutOptions(
+      1.00,
+      "EGP",
+      callbackUrl: "https://website.hook/", 
+      returnUrl: "https://returnurl.com",
+      lang: "AR", 
+      billingAddress: billingAddress,
+      shippingAddress: shippingAddress,
+      customerEmail: "email@noreply.test",
+      merchantReferenceID: "12345",
+      paymentOperation: "Pay", 
+      showAddress: true, 
+      showEmail: true, 
+      textColor: const Color(0xffffffff), 
+      cardColor: const Color(0xffff4d00), 
+      payButtonColor: const Color(0xffff4d00), 
+      cancelButtonColor: const Color(0xff878787),
+      backgroundColor: const Color(0xff2c2222),
+    );
+
+    try {
+      OrderApiResponse response = await _plugin.checkout(
+        context: context,
+        checkoutOptions: checkoutOptions,
+      );
+
+      _updateStatus(response.detailedResponseMessage!, response.toString());
+    } catch (e) {
+      _showMessage(context, "Error: $e");
+    }
+  }
+
+  void _updateStatus(String status, String responseMessage) {
+    debugPrint("Status: $status");
+    debugPrint("Response: $responseMessage");
+  }
+
+  void _showMessage(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/payment_screen.dart
+++ lib/payment_screen.dart
@@ -1,97 +1,97 @@
- import 'package:flutter/material.dart';
-import 'package:geideapay/api/response/order_api_response.dart';
-import 'package:geideapay/common/geidea.dart';
-import 'package:geideapay/models/address.dart';
-import 'package:geideapay/widgets/checkout/checkout_options.dart';
+//  import 'package:flutter/material.dart';
+// import 'package:geideapay/api/response/order_api_response.dart';
+// import 'package:geideapay/common/geidea.dart';
+// import 'package:geideapay/models/address.dart';
+// import 'package:geideapay/widgets/checkout/checkout_options.dart';
 
-class PaymentScreen extends StatelessWidget {
-  final GeideapayPlugin _plugin = GeideapayPlugin();
+// class PaymentScreen extends StatelessWidget {
+//   final GeideapayPlugin _plugin = GeideapayPlugin();
 
-  PaymentScreen({super.key});
+//   PaymentScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    // Define billing and shipping address details
-    Address billingAddress = Address(
-      city: "Riyadh",
-      countryCode: "SAU",
-      street: "Street 1",
-      postCode: "1000",
-    );
+//   @override
+//   Widget build(BuildContext context) {
+//     // Define billing and shipping address details
+//     Address billingAddress = Address(
+//       city: "Riyadh",
+//       countryCode: "SAU",
+//       street: "Street 1",
+//       postCode: "1000",
+//     );
 
-    Address shippingAddress = Address(
-      city: "Riyadh",
-      countryCode: "SAU",
-      street: "Street 1",
-      postCode: "1000",
-    );
+//     Address shippingAddress = Address(
+//       city: "Riyadh",
+//       countryCode: "SAU",
+//       street: "Street 1",
+//       postCode: "1000",
+//     );
 
-    // Set up checkout options
-    CheckoutOptions checkoutOptions = CheckoutOptions(
-      123.45,
-      "SAR",
-      callbackUrl: "https://website.hook/", 
-      returnUrl: "https://returnurl.com", 
-      lang: "AR",
-      billingAddress: billingAddress, 
-      shippingAddress: shippingAddress, 
-      customerEmail: "email@noreply.test", 
-      merchantReferenceID: "1234", 
-      paymentOperation: "Pay", 
-      showAddress: true, 
-      showEmail: true, 
-      textColor: const Color(0xffffffff), 
-      cardColor: const Color(0xffff4d00), 
-      payButtonColor: const Color(0xffff4d00), 
-      cancelButtonColor: const Color(0xff878787),
-      backgroundColor: const Color(0xff2c2222),
-    );
+//     // Set up checkout options
+//     CheckoutOptions checkoutOptions = CheckoutOptions(
+//       123.45,
+//       "SAR",
+//       callbackUrl: "https://website.hook/", 
+//       returnUrl: "https://returnurl.com", 
+//       lang: "AR",
+//       billingAddress: billingAddress, 
+//       shippingAddress: shippingAddress, 
+//       customerEmail: "email@noreply.test", 
+//       merchantReferenceID: "1234", 
+//       paymentOperation: "Pay", 
+//       showAddress: true, 
+//       showEmail: true, 
+//       textColor: const Color(0xffffffff), 
+//       cardColor: const Color(0xffff4d00), 
+//       payButtonColor: const Color(0xffff4d00), 
+//       cancelButtonColor: const Color(0xff878787),
+//       backgroundColor: const Color(0xff2c2222),
+//     );
 
-    return Scaffold(
-      appBar: AppBar(title: const Text('Payment Screen')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              OrderApiResponse response = await _plugin.checkout(
-                  context: context, checkoutOptions: checkoutOptions);
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Payment Screen')),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () async {
+//             try {
+//               OrderApiResponse response = await _plugin.checkout(
+//                   context: context, checkoutOptions: checkoutOptions);
 
-              debugPrint('Response = $response');
-              // Handle successful payment response
-              _updateStatus(
-                  response.detailedResponseMessage!, response.toString());
-            } catch (e) {
-              debugPrint("Error: $e");
-              _showMessage(context,e.toString());
-            }
-          },
-          child: const Text('Proceed to Checkout'),
-        ),
-      ),
-    );
-  }
+//               debugPrint('Response = $response');
+//               // Handle successful payment response
+//               _updateStatus(
+//                   response.detailedResponseMessage!, response.toString());
+//             } catch (e) {
+//               debugPrint("Error: $e");
+//               _showMessage(context,e.toString());
+//             }
+//           },
+//           child: const Text('Proceed to Checkout'),
+//         ),
+//       ),
+//     );
+//   }
 
-  void _updateStatus(String status, String responseMessage) {
-    // Update UI with response status
-    debugPrint("Status: $status");
-    debugPrint("Response: $responseMessage");
-  }
+//   void _updateStatus(String status, String responseMessage) {
+//     // Update UI with response status
+//     debugPrint("Status: $status");
+//     debugPrint("Response: $responseMessage");
+//   }
 
-  void _showMessage(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Error'),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
+//   void _showMessage(BuildContext context, String message) {
+//     showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           title: const Text('Error'),
+//           content: Text(message),
+//           actions: <Widget>[
+//             TextButton(
+//               onPressed: () => Navigator.of(context).pop(),
+//               child: const Text('OK'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
